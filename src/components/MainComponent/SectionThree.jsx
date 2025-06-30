391a19950f635bf26103da0717999645d7e2584e
Rahat <rahatahmedbosscomputer@gmail.com>
2025-06-30 09:14:19 +0600

    unnecessary import fix

--- src/components/MainComponent/SectionThree.jsx
+++ src/components/MainComponent/SectionThree.jsx
@@ -1,4 +1,4 @@
-import { delay, motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 import SectionThreeTopic from './SectionThreeTopic'
 import { useRef } from 'react'
 import { useInView } from 'framer-motion'
