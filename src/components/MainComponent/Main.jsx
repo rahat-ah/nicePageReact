fc99af1a36737b9fd86a9e5dc2c7ccb85bd3e47c
Rahat <rahatahmedbosscomputer@gmail.com>
2025-06-30 15:13:10 +0600

    case sensitive importing component issue solved

--- src/components/MainComponent/Main.jsx
+++ src/components/MainComponent/Main.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import SectionOne from "./SectionOne"
 import SectionTwo from "./SectionTwo"
-import SectionThree from "./sectionThree"
+import SectionThree from "./SectionThree"
 import SectionFour from "./SectionFour"
 import SectionFive from "./SectionFive"
 import SectionSix from "./SectionSix"
